1b00aaa698af436bdecfef8c5d5888ab1f7c57a6
OlamideHawanat123 <raheemhawanat>
2026-03-16 21:51:23 +0100

    first commit

---
+++ src/main/java/project/hawanah/walletex/WalletExApplication.java
@@ -0,0 +1,13 @@
+package project.hawanah.walletex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/hawanah/walletex/WalletExApplicationTests.java
@@ -0,0 +1,13 @@
+package project.hawanah.walletex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
